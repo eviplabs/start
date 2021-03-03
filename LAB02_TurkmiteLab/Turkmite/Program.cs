ae53b565ca5a17e1202825f1870674754497ac28
nope <nope@localhost.localdomain>
2021-03-03 01:30:31 +0100

    Three Color Turkmite added

--- LAB02_TurkmiteLab/Turkmite/Program.cs
+++ LAB02_TurkmiteLab/Turkmite/Program.cs
@@ -2,6 +2,36 @@
 
 namespace TurkMite
 {
+
+  class TurkmiteThreeColor: TurkmiteBase
+  {
+    readonly private Vec3b black = new Vec3b(0, 0, 0);
+    readonly private Vec3b white = new Vec3b(255, 255, 255);
+    readonly private Vec3b red = new Vec3b(255, 0, 0);
+    public TurkmiteThreeColor(Mat image) : base(image)
+    {
+
+    }
+    protected override (Vec3b newColor, int deltaDirection) NextDirectionColor(Vec3b currentColor)
+    {
+      if(currentColor == black)
+      {
+        return(white, 1);
+      }
+      else if (currentColor == white)
+      {
+        return (red, -1);
+      }
+      else
+      {
+        return (black, -1);
+      }
+
+
+    }
+
+
+  }
   class TurkMiteOriginal : TurkmiteBase
   {
 
