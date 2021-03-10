8af7d04e5a3defa982129c404c9daabc96ddaec0
szelen97 <63542052+szelen97@users.noreply.github.com>
2021-03-10 15:37:48 +0100

    8. Feladat: SurrenderBooster készítése
    A surrender gomb megnyomásakor a másik játékos lesz a győztes (winner).
    Két for ciklus segítségével végigmegyek az egész táblán, és a még kitöltetlen, üres mezőket a winner színével színezem ki.

--- LAB10_AttaxxPlus/AttaxxPlus/Boosters/SurrenderBooster.cs
+++ LAB10_AttaxxPlus/AttaxxPlus/Boosters/SurrenderBooster.cs
@@ -13,11 +13,22 @@ namespace AttaxxPlus.Boosters
 
         public SurrenderBooster() : base()
         {
+            LoadImage(new Uri(@"ms-appx:///Boosters/SurrenderBooster.png"));
         }
 
         public override bool TryExecute(Field selectedField, Field currentField)
         {
-            return false;
+            int winner = (GameViewModel.CurrentPlayer == 1) ? 2 : 1;
+
+            for (int row = 0; row < GameViewModel.Fields.Count; row++)
+            {
+                for (int col = 0; col < GameViewModel.Fields.Count; col++)
+                {
+                    if(GameViewModel.Model.Fields[row, col].IsEmpty()) GameViewModel.Model.Fields[row, col].Owner = winner;
+                }
+            }
+
+            return true;
         }
     }
 }
