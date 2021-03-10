1d82f56ada7e22241cee6472e960aade808df934
Tóth Mátyás <62833377+mattyer282@users.noreply.github.com>
2021-03-10 13:17:31 +0100

    9. feladat

---
+++ LAB10_AttaxxPlus/AttaxxPlus/Boosters/RowBooster.cs
@@ -0,0 +1,51 @@
+﻿using AttaxxPlus.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttaxxPlus.Boosters
+{
+    class RowBooster : BoosterBase
+    {
+
+        private int[] playerCounter = { 0, 1, 1 };
+        public override string Title { get => $"Set Row ({playerCounter[GameViewModel.CurrentPlayer]})"; }
+
+        public RowBooster() : base()
+        { }
+
+        protected override void CurrentPlayerChanged()
+        {
+            base.CurrentPlayerChanged();
+            Notify(nameof(this.Title));
+        }
+
+        public override void InitializeGame()
+        {
+            playerCounter[1] = 1;
+            playerCounter[2] = 1;
+        }
+
+        public override bool TryExecute(Field selectedField, Field currentField)
+        {
+            if (selectedField == null)
+            {
+                return false;
+            }
+            else
+            {
+                if (playerCounter[GameViewModel.CurrentPlayer] > 0)
+                {
+                    playerCounter[GameViewModel.CurrentPlayer]--;
+                    Notify(nameof(Title));
+                    for (int row = 0; row < GameViewModel.Model.Fields.GetLength(1); row++)
+                        GameViewModel.Model.Fields[row, selectedField.Column].Owner = 0;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
