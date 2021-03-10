cc02ccd3bf9786196d7d60df343a7c1f26daa2ed
bb4lu <balogh.balazs97@gmail.com>
2021-03-10 14:09:02 +0100

    Extra task done

--- LAB10_AttaxxPlus/AttaxxPlus/Boosters/UltimateBooster.cs
+++ LAB10_AttaxxPlus/AttaxxPlus/Boosters/UltimateBooster.cs
@@ -8,22 +8,46 @@ namespace AttaxxPlus.Boosters
     /// </summary>
     public class UltimateBooster : BoosterBase
     {
+        // How many times can users activate this booster
+        private int[] usableCounter = { -1, 1, 1 };
+
         // EVIP: compact override of getter for Title returning constant.
-        public override string Title => "Ultimate";
+        public override string Title { get => $"Ultimate ({usableCounter[this.GameViewModel.CurrentPlayer]})"; }
 
         public UltimateBooster() : base()
         {
             LoadImage(new Uri(@"ms-appx:///Boosters/UltimateBooster.png"));
         }
-
+        protected override void CurrentPlayerChanged()
+        {
+            base.CurrentPlayerChanged();
+            Notify(nameof(this.Title));
+        }
+        public override void InitializeGame()
+        {
+            usableCounter[0] = -1;
+            usableCounter[1] = 1;
+            usableCounter[2] = 1;
+        }
         public override bool TryExecute(Field selectedField, Field currentField)
         {
+            selectedField = this.GameViewModel.SelectedField.Model;
             var currPlayer = this.GameViewModel.Model.CurrentPlayer;
-            if (selectedField.Owner == currPlayer)
+            if (usableCounter[currPlayer] > 0)
             {
-
-                return true;
+                if ((!selectedField.IsEmpty()) && (selectedField.Owner == currPlayer))
+                {
+                    foreach (var currField in this.GameViewModel.Model.Fields)
+                    {
+                        if ((currField.Column == selectedField.Column) || (currField.Row == selectedField.Row))
+                            currField.Owner = currPlayer;
+                    }
+                    usableCounter[currPlayer]--;
+                    Notify(nameof(Title));
+                    return true;
+                }
             }
+            
             return false;
         }
     }
