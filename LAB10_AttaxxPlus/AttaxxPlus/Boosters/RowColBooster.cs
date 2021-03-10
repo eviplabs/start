75b28f01fc7d742d897c2cc1e99fc3a6c60bbe8b
Vagner Jázon <137813@gmail.com>
2021-03-10 11:09:31 +0100

    9.: Sort és oszlopot törlő booster És egy kis javítás a DummyBoosterben

---
+++ LAB10_AttaxxPlus/AttaxxPlus/Boosters/RowColBooster.cs
@@ -0,0 +1,54 @@
+﻿using AttaxxPlus.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttaxxPlus.Boosters
+{
+    public class RowColBooster : BoosterBase
+    {
+        private int[] usableCounter = new int[3];
+
+        public override string Title => $"DestroyRowCol ({usableCounter[this.GameViewModel.CurrentPlayer]})";
+
+        public RowColBooster() : base()
+        {
+            LoadImage(new Uri(@"ms-appx:///Boosters/bomb.png"));
+        }
+
+        public override bool TryExecute(Field selectedField, Field currentField)
+        {
+            if ((selectedField is null)
+                || (selectedField.Owner != this.GameViewModel.CurrentPlayer)
+                || (usableCounter[this.GameViewModel.CurrentPlayer] == 0))
+                return false;
+
+            usableCounter[this.GameViewModel.CurrentPlayer]--;
+            Notify(nameof(Title));
+
+            int row = selectedField.Row;
+            int col = selectedField.Column;
+            foreach (var f in this.GameViewModel.Model.Fields)
+                if ((f.Row == row) || (f.Column == col))
+                    f.Owner = 0;
+
+            return true;
+        }
+
+        public override void InitializeGame()
+        {
+            usableCounter[0] = 0;
+            usableCounter[1] = 1;
+            usableCounter[2] = 1;
+            Notify(nameof(this.Title));
+        }
+
+        protected override void CurrentPlayerChanged()
+        {
+            base.CurrentPlayerChanged();
+            Notify(nameof(this.Title));
+        }
+    }
+}
