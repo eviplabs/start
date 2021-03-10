5122e4a05322a4634e18cfa7da0bb52d49cf184f
Vagner Jázon <137813@gmail.com>
2021-03-10 10:27:52 +0100

    8.: SurrenderBooster

--- LAB10_AttaxxPlus/AttaxxPlus/Boosters/SurrenderBooster.cs
+++ LAB10_AttaxxPlus/AttaxxPlus/Boosters/SurrenderBooster.cs
@@ -13,11 +13,18 @@ namespace AttaxxPlus.Boosters
 
         public SurrenderBooster() : base()
         {
+            LoadImage(new Uri(@"ms-appx:///Boosters/SurrenderBooster.png"));
         }
 
         public override bool TryExecute(Field selectedField, Field currentField)
         {
-            return false;
+            int otherPlayer = (this.GameViewModel.CurrentPlayer == 1) ? 2 : 1;
+
+            foreach (var f in this.GameViewModel.Model.Fields)
+                if (f.IsEmpty())
+                    f.Owner = otherPlayer;
+
+            return true;
         }
     }
 }
