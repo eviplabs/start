d77028f0ceb1e980ae782b3cc9accf7042b0e822
Baracsk <Baracsk@users.noreply.github.com>
2021-03-10 08:51:28 +0100

    1. feladat: a keret színét sárgáról indigóra változtattam

--- LAB10_AttaxxPlus/AttaxxPlus/View/IsSelected2BrushConverter.cs
+++ LAB10_AttaxxPlus/AttaxxPlus/View/IsSelected2BrushConverter.cs
@@ -8,12 +8,13 @@ namespace AttaxxPlus.View
     public class IsSelected2BrushConverter : IValueConverter
     {
         // EVIP: reusing brushes, named constants
-        readonly private static SolidColorBrush yellow = new SolidColorBrush(Colors.Yellow);
+        //1. feladat: keret szín megváltoztatása
+        readonly private static SolidColorBrush indigo = new SolidColorBrush(Colors.Indigo);
         readonly private static SolidColorBrush gray = new SolidColorBrush(Colors.Gray);
 
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            return ((bool)value) ? yellow : gray;
+            return ((bool)value) ? indigo : gray;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
