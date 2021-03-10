0e99d805aa410aa5c55f9f38bed46862304b7359
Baracsk <Baracsk@users.noreply.github.com>
2021-03-10 10:44:59 +0100

    8. feladat: surrender booster készítése

--- LAB10_AttaxxPlus/AttaxxPlus/Boosters/SurrenderBooster.cs
+++ LAB10_AttaxxPlus/AttaxxPlus/Boosters/SurrenderBooster.cs
@@ -11,13 +11,31 @@ namespace AttaxxPlus.Boosters
         // EVIP: compact override of getter for Title returning constant.
         public override string Title => "Surrender";
 
+        //8. feladat: surrender booster képe
         public SurrenderBooster() : base()
         {
+            LoadImage(new Uri(@"ms-appx:///Boosters/SurrenderBooster.png"));
+        }
+
+        protected override void CurrentPlayerChanged()
+        {
+            base.CurrentPlayerChanged();
+            Notify(nameof(this.Title));
         }
 
         public override bool TryExecute(Field selectedField, Field currentField)
         {
-            return false;
+            //8. feladat: surrender esetén a másik játékos megkapja az összes mezőt
+            int NewOwner = (GameViewModel.CurrentPlayer == 1) ? 2 : 1;
+
+            for (int i = 0; i < GameViewModel.Fields.Count; i++)
+            {
+                for (int j = 0; j< GameViewModel.Fields.Count; j++)
+                {
+                    GameViewModel.Model.Fields[i, j].Owner = NewOwner;
+                }
+            }
+            return true;
         }
     }
 }
