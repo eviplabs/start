310d56a615ab18b6fd9c8731d1198935826b76cc
mtorok97 <mtorok@edu.bme.hu>
2021-03-14 16:53:57 +0100

    Szorgalmi feladat: új booster
    Létrehoztam egy EmptyBooster nevű osztályt, amely a BoosterBaseből származik le. Ebben megvalósítom, azt hogy mindkét játékosnak lehetősége van egyszer kiüresíteni azt a sort és oszlopot ahol ki van jelölve a mezője.

---
+++ LAB10_AttaxxPlus/AttaxxPlus/Boosters/EmptyBooster.cs
@@ -0,0 +1,51 @@
+﻿using AttaxxPlus.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttaxxPlus.Boosters
+{
+    public class EmptyBooster: BoosterBase
+    {
+        private int[] usableCounter = { 0, 1, 1 };
+        public override string Title => $"Clean ({usableCounter[GameViewModel.CurrentPlayer]})";
+
+        public EmptyBooster() : base()
+        {
+            LoadImage(new Uri(@"ms-appx:///Boosters/EmptyBooster.png"));
+        }
+
+        public override void InitializeGame()
+        {
+            usableCounter[1] = 1;
+            usableCounter[2] = 1;
+        }
+
+        protected override void CurrentPlayerChanged()
+        {
+            base.CurrentPlayerChanged();
+            Notify(nameof(this.Title));
+        }
+
+        public override bool TryExecute(Field selectedField, Field currentField)
+        {
+            if (usableCounter[GameViewModel.CurrentPlayer] > 0 && selectedField != null)
+            {
+                usableCounter[GameViewModel.CurrentPlayer]--;
+                Notify(nameof(Title));
+                foreach (Field field in GameViewModel.Model.Fields)
+                {
+                    if (field.Row == selectedField.Row || field.Column == selectedField.Column)
+                        field.Owner = 0;
+                }
+
+                return true;
+
+
+            }
+            return false;
+        }
+    }
+}
