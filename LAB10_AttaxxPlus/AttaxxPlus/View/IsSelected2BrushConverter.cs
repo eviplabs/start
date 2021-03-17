b9249d1704d1d52ebda5e4c9f9d99c6ff7e5fe21
Roland Sipos <siposroland97@gmail.com>
2021-03-17 20:35:08 +0100

    Task1 Selected field color changed from yellow to blue

--- LAB10_AttaxxPlus/AttaxxPlus/View/IsSelected2BrushConverter.cs
+++ LAB10_AttaxxPlus/AttaxxPlus/View/IsSelected2BrushConverter.cs
@@ -9,11 +9,12 @@ namespace AttaxxPlus.View
     {
         // EVIP: reusing brushes, named constants
         readonly private static SolidColorBrush yellow = new SolidColorBrush(Colors.Yellow);
+        readonly private static SolidColorBrush blue = new SolidColorBrush(Colors.Blue);
         readonly private static SolidColorBrush gray = new SolidColorBrush(Colors.Gray);
 
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            return ((bool)value) ? yellow : gray;
+            return ((bool)value) ? blue : gray;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
