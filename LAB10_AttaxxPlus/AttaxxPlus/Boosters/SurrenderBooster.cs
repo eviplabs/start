4448a9841cba2576523b136e581ca997b6da9b00
oprezz <antal.marci@gmail.com>
2021-03-17 11:58:01 +0100

    8. SurrenderBooster keszitese

--- LAB10_AttaxxPlus/AttaxxPlus/Boosters/SurrenderBooster.cs
+++ LAB10_AttaxxPlus/AttaxxPlus/Boosters/SurrenderBooster.cs
@@ -13,11 +13,33 @@ namespace AttaxxPlus.Boosters
 
         public SurrenderBooster() : base()
         {
+            // EVIP: referencing content resource with Uri.
+            //  The image is added to the project as "Content" build action.
+            //  See also for embedded resources: https://docs.microsoft.com/en-us/windows/uwp/app-resources/
+            // https://docs.microsoft.com/en-us/windows/uwp/app-resources/images-tailored-for-scale-theme-contrast#reference-an-image-or-other-asset-from-xaml-markup-and-code
+            LoadImage(new Uri(@"ms-appx:///Boosters/SurrenderBooster.png"));
         }
+
+        protected override void CurrentPlayerChanged()
+        {
+            base.CurrentPlayerChanged();
+            Notify(nameof(this.Title));
+        }
+
 
         public override bool TryExecute(Field selectedField, Field currentField)
         {
-            return false;
+            if (this.GameViewModel.CurrentPlayer < this.GameViewModel.Model.NumberOfPlayers)
+                this.GameViewModel.Model.CurrentPlayer++;
+            else
+                this.GameViewModel.Model.CurrentPlayer = 1;
+
+            foreach (Field field in this.GameViewModel.Model.Fields)
+            {
+                if (field.IsEmpty())
+                    field.Owner = this.GameViewModel.CurrentPlayer;
+            }
+            return true;
         }
     }
 }
