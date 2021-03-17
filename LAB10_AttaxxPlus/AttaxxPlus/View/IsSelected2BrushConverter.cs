198f545e4ab8dc4566a271cb6c6d66d6e600caa7
tszab <szabolcs.torma96@gmail.com>
2021-03-17 10:18:40 +0100

    Task1: color change

--- LAB10_AttaxxPlus/AttaxxPlus/View/IsSelected2BrushConverter.cs
+++ LAB10_AttaxxPlus/AttaxxPlus/View/IsSelected2BrushConverter.cs
@@ -8,12 +8,12 @@ namespace AttaxxPlus.View
     public class IsSelected2BrushConverter : IValueConverter
     {
         // EVIP: reusing brushes, named constants
-        readonly private static SolidColorBrush yellow = new SolidColorBrush(Colors.Yellow);
+        readonly private static SolidColorBrush lightGreen = new SolidColorBrush(Colors.LightGreen);
         readonly private static SolidColorBrush gray = new SolidColorBrush(Colors.Gray);
 
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            return ((bool)value) ? yellow : gray;
+            return ((bool)value) ? lightGreen : gray;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
