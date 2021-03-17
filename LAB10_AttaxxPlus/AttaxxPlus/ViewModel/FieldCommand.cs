5d70f2b0f52a0393d993698c1fc3e52f437a32c6
Lia Erika Babud <lia.babud@edu.bme.hu>
2021-03-18 00:35:52 +0100

    3.feladat Modify field selection

--- LAB10_AttaxxPlus/AttaxxPlus/ViewModel/FieldCommand.cs
+++ LAB10_AttaxxPlus/AttaxxPlus/ViewModel/FieldCommand.cs
@@ -32,6 +32,7 @@ namespace AttaxxPlus.ViewModel
             // Clicking own field selects that field.
             if (current.Owner == vm.CurrentPlayer)
             {
+                current.IsSelected = true;
                 vm.SelectedField = current;
                 return;
             }
