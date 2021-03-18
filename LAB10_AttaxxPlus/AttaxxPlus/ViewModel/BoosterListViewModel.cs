27b0b623b2637d21ba02faa99c82eceb70ecebf3
PickleRick9608 <36385991+PickleRick9608@users.noreply.github.com>
2021-03-18 20:21:01 +0100

    5 feladat
    A probléma az volt, hogy ugyan létre lett hozva az új boosters példány, de nem lett hozzáadva a listához. (Ha hozzáadjuk, akkor már látszódik.)

--- LAB10_AttaxxPlus/AttaxxPlus/ViewModel/BoosterListViewModel.cs
+++ LAB10_AttaxxPlus/AttaxxPlus/ViewModel/BoosterListViewModel.cs
@@ -24,6 +24,8 @@ namespace AttaxxPlus.ViewModel
                 // EVIP: using reflection to instantiate objects
                 IBooster booster = Activator.CreateInstance(boosterType.AsType()) as IBooster;
                 booster.GameViewModel = gameViewModel;
+                // add the new element to the list !
+                Boosters.Add(booster);
             }
         }
     }
