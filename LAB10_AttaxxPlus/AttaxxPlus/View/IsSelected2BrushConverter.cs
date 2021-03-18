031ee3507df5a5c7436c06dec679d6c115379fd7
baranbeni <baranbeni96@gmail.com>
2021-03-19 00:25:00 +0100

    Changed margin colour to black.

--- LAB10_AttaxxPlus/AttaxxPlus/View/IsSelected2BrushConverter.cs
+++ LAB10_AttaxxPlus/AttaxxPlus/View/IsSelected2BrushConverter.cs
@@ -10,10 +10,12 @@ namespace AttaxxPlus.View
         // EVIP: reusing brushes, named constants
         readonly private static SolidColorBrush yellow = new SolidColorBrush(Colors.Yellow);
         readonly private static SolidColorBrush gray = new SolidColorBrush(Colors.Gray);
+        //1.Feladat
+        readonly private static SolidColorBrush newcolour = new SolidColorBrush(Colors.Black);
 
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            return ((bool)value) ? yellow : gray;
+            return ((bool)value) ? newcolour : gray;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
