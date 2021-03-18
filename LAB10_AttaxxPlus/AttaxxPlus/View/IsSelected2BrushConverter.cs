bc84d722f96ba94d9440f834882061f6effb5519
mate0117 <55443550+mate0117@users.noreply.github.com>
2021-03-18 19:02:25 +0100

    1. feladat
    Border color changed form yellow to red, if it is selected

--- LAB10_AttaxxPlus/AttaxxPlus/View/IsSelected2BrushConverter.cs
+++ LAB10_AttaxxPlus/AttaxxPlus/View/IsSelected2BrushConverter.cs
@@ -9,11 +9,12 @@ namespace AttaxxPlus.View
     {
         // EVIP: reusing brushes, named constants
         readonly private static SolidColorBrush yellow = new SolidColorBrush(Colors.Yellow);
+        readonly private static SolidColorBrush red = new SolidColorBrush(Colors.DarkRed);
         readonly private static SolidColorBrush gray = new SolidColorBrush(Colors.Gray);
 
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            return ((bool)value) ? yellow : gray;
+            return ((bool)value) ? red : gray;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
