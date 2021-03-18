05fb06ea6501a8f03f9a233e43628069fa2a5bf4
PickleRick9608 <36385991+PickleRick9608@users.noreply.github.com>
2021-03-18 22:22:05 +0100

    Szorgalma part1

---
+++ LAB10_AttaxxPlus/AttaxxPlus/Boosters/CrossBooster.cs
@@ -0,0 +1,44 @@
+﻿using AttaxxPlus.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttaxxPlus.Boosters
+{
+	public class CrossBooster : BoosterBase
+	{
+		private int[] usableCounters = { 0, 0, 0 };
+
+
+		public CrossBooster() :base()
+		{
+			LoadImage(new Uri(@"ms-appx:///Boosters/CrossBoosters.png"));
+
+		}
+
+		public override void InitializeGame()
+		{
+			usableCounters = new int[] { 0, 1, 1 };
+		}
+
+		protected override void CurrentPlayerChanged()
+		{
+			base.CurrentPlayerChanged(); Notify(nameof(Title));
+		}
+
+
+		public override string Title {
+			get {
+				return $"Cross Booster({usableCounters[GameViewModel.CurrentPlayer]})";
+			} 
+		}
+
+
+		public override bool TryExecute(Field selectedField, Field currentField)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
