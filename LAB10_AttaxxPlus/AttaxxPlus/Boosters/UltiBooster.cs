fa58efb5262fbf22cde42717cdbafd68071baec5
baranbeni <baranbeni96@gmail.com>
2021-03-19 02:14:17 +0100

    Szorgalmi feladat
    Az UltiBooster.cs fájlban valósítottam meg a leírásban szereplő booster működését. A gomb megnyomásakor megvizsgálja, hogy van e kiválasztva mező, és ha igen, akkor annak tulajdonosa megegyezik e az aktív játékossal (ez utóbbi egyébként mindig igaz lesz), és ha igen, akkor az adott soron és oszlopon végighaladva semleges mezőkké változtatja azok elemeit. Illetve eláttam egy ugyanolyan számlálóval, mint a dummy bosster esetén, így mindegyik játékos csak egyszer tudja használni.

---
+++ LAB10_AttaxxPlus/AttaxxPlus/Boosters/UltiBooster.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using AttaxxPlus.Model;
+
+namespace AttaxxPlus.Boosters
+{
+    /// <summary>
+    /// Booster clearing all fileds in a row and coloumn determined by a player's own field (assuming 2 players).
+    /// </summary>
+    public class UltiBooster : BoosterBase
+    {
+        // How many times can the user activate this booster
+        private int[] usableCounter = { 0, 1, 1 };
+
+        // EVIP: overriding abstract property in base class.
+        public override string Title { get => $"Ultimate Power ({usableCounter[this.GameViewModel.CurrentPlayer]})"; }
+
+        public UltiBooster() : base()
+        {
+            LoadImage(new Uri(@"ms-appx:///Boosters/UltiBooster.png"));
+        }
+
+        protected override void CurrentPlayerChanged()
+        {
+            base.CurrentPlayerChanged();
+            Notify(nameof(this.Title));
+        }
+
+        public override void InitializeGame()
+        {
+            usableCounter[1] = 1;
+            usableCounter[2] = 1;
+        }
+
+        public override bool TryExecute(Field selectedField, Field currentField)
+        {
+            //9. Feladat
+            if (GameViewModel.SelectedField == null)
+            {
+                return false;
+            }
+            if (usableCounter[this.GameViewModel.CurrentPlayer] > 0)
+            {
+                if (GameViewModel.SelectedField.Owner == GameViewModel.CurrentPlayer)
+                {
+                    for (int i = 0; i < this.GameViewModel.Fields.Count; i++)
+                    {
+                        this.GameViewModel.Model.Fields[this.GameViewModel.SelectedField.Model.Row, i].Owner = 0;
+                        this.GameViewModel.Model.Fields[i, this.GameViewModel.SelectedField.Model.Column].Owner = 0;
+                    }
+                }
+                usableCounter[this.GameViewModel.CurrentPlayer]--;
+                Notify(nameof(Title));
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
